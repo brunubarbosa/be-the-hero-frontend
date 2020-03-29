2c33485a977bd195ce35559ac505ddaf2dd2a7b9
brunubarbosa <brunopjsw@gmail.com>
2020-03-29 20:56:37 -0300

    logon datail and profile

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -4,7 +4,7 @@ import React, {useState} from 'react';
 import {Link, useHistory} from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi';
 
-// import api from '../../services/api'
+import api from '../../services/api';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
@@ -12,15 +12,28 @@ import heroesImg from '../../assets/heroes.png';
 
 
 export default function Logon() {
+  const [id, setId] = useState('');
+  const history = useHistory()
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    const {data} = await api.post('login', {id})
+    if(data.name) {
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', data.name);
+      history.push('/profile');
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
 
-        <form onSubmit={'handleLogin'}>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Sua ID" value={'id'} onChange={'e => setId(e.target.value)'}/>
+          <input placeholder="Sua ID" value={id} onChange={e => setId(e.target.value)}/>
           <button className="button" type="submit">
             Entrar
           </button>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -6,59 +6,59 @@ import { FiPower , FiTrash2} from 'react-icons/fi';
 import './styles.css'
 
 import logoImg from '../../assets/logo.svg';
-// import api from '../../services/api';
+import api from '../../services/api';
 
 export default function Profile(){
-  // const [incidents, setIncidents] = useState([])
+  const [incidents, setIncidents] = useState([])
 
-  // const history = useHistory()
+  const history = useHistory()
 
-  // const ongName = localStorage.getItem('ongName')
-  // const ongId = localStorage.getItem('ongId')
+  const ongName = localStorage.getItem('ongName')
+  const ongId = localStorage.getItem('ongId')
 
-  // useEffect(()=>{
-  //   api.get('profile', {
-  //     headers:{
-  //       Authorization: ongId
-  //     }
-  //   }).then(response => setIncidents(response.data))
-  //     .catch(err => alert(err))
-  // }, [ongId])
+  useEffect(()=>{
+    api.get('profile', {
+      headers:{
+        Authorization: ongId
+      }
+    }).then(response => setIncidents(response.data))
+      .catch(err => alert(err))
+  }, [ongId])
   
-  // async function handleDeleteIncident(id){
-  //   try{
-  //     await api.delete(`incidents/${id}`, {
-  //       headers: {
-  //         Authorization: ongId
-  //       }
-  //     })
+  async function handleDeleteIncident(id){
+    try{
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId
+        }
+      })
 
-  //     setIncidents(incidents.filter(incident => incident.id !== id))
-  //   }catch(err){
-  //     alert('Erro ao deletar caso, tente novamente.')
-  //   }
-  // }
+      setIncidents(incidents.filter(incident => incident.id !== id))
+    }catch(err){
+      alert('Erro ao deletar caso, tente novamente.')
+    }
+  }
 
-  // function handleLogout(){
-  //   localStorage.clear()
-  //   history.push('/')
-  // }
+  function handleLogout(){
+    localStorage.clear()
+    history.push('/')
+  }
 
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be The Hero" />
-        <span>Bem Vinda, {'ongName'}</span>
+        <span>Bem Vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-        <button type="button" onClick={'handleLogout'}>
+        <button type="button" onClick={handleLogout}>
           <FiPower size={18} color="#e02041"/>
         </button>
       </header>
 
       <h1>Casos Cadastrados</h1>
 
-      {/* <ul>
+      <ul>
         {incidents.map(incident => (
           <li key={incident.id}>
             <strong>Caso:</strong>
@@ -75,7 +75,7 @@ export default function Profile(){
           </button>
         </li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   )
 }
